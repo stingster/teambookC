154c43c59885c1700c85fad151fb44089ee2ef9a
stingster <mirceacristea@yahoo.co.uk>
2016-07-05 11:48:52 +0300

    Test pt Cristina

--- src/main/java/controller/Test.java
+++ src/main/java/controller/Test.java
@@ -1,5 +1,7 @@
 package controller;
 
+import domain.Player;
+import domain.PlayerEmployee;
 
 public class Test
 {
@@ -7,7 +9,7 @@ public class Test
 	public static void main(String[] args)
 	{
 
-	
+	Player player = new PlayerEmployee();
 
 
 	}
